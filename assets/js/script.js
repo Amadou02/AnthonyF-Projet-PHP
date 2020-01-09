93cf6b74da29e2aadb0c2bed38d4b026e8a8e262
Tonyio83 <anthonyfrancelle@gmail.com>
2020-01-09 16:44:34 +0100

    envoi du js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 $(function(){
    $('iframe').parent('p').wrap('<div class="container-fluid"></div>');
+   $('.gallery_box_single').wrap('<div class="carousel slide" data-ride="carousel" role="listbox"></div>');
+   $('.gallery_box_single').addClass('carousel-inner');
+   $('.img:first-child').addClass('active');
+   $('.img').addClass('carousel-item d-block');
+   $('.img img').addClass('w-100');
    $('form').append('<input class="btn btn-dark mt-3" type="submit" value="Envoyer">');
+   
 });
