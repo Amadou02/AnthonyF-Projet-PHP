c9963ccadf78b584f76fe3196468e038572607d8
EmilieLamps <emilie.lamps@gmail.com>
2020-01-10 11:45:08 +0100

    Modification tailles p

--- index.php
+++ index.php
@@ -12,13 +12,6 @@ $pageTitle = '';
 if (isset($_GET) && !empty($_GET)) {
     $idPage = $_GET['id'];
 }
-//Redirection si on entre index.php dans l'url
-elseif (empty($_GET['id'])) {
-    $idPage = '1';
-    $monUrl = 'http://maconnerieocordo.com/1.html';
-    header($monUrl);
-}
-include 'validation.php';
 ?>
 <!doctype html>
 <html lang="fr">
@@ -26,17 +19,17 @@ include 'validation.php';
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/page<?= $idPage ?>.css">  
-        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/page<?= $idPage ?>.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> 
         <title><?= $pages->page[$idPage - 1]->title ?></title>
     </head>
     <body>
-        <header>
-            <div class="container m-auto">
-                <img src="assets/img/Logo-Ocordo-Travaux-Amiens.png" alt="logo ocordo maçonnerie" id="logo" href="">
-                <img src="assets/img/headerimg.jpg" alt="illustrations rénovation de maison" id="headerimage">
-            </div>
-        </header>
+    <header>
+        <div class="container m-auto">
+            <img src="assets/img/Logo-Ocordo-Travaux-Amiens.png" alt="logo ocordo maçonnerie" id="logo" href="">
+            <img src="assets/img/headerimg.jpg" alt="illustrations rénovation de maison" id="headerimage">
+        </div>
+    </header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-center">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -54,21 +47,12 @@ include 'validation.php';
                 </ul>
             </div>
         </nav>
-        <div class="jumbotron-fluid">
-            <?php
-            //si post du formulaire complété sans erreurs, affichage du résultat
-            if ($isSubmitted && count($errors) == 0 && $idPage == 4) {
-                ?>
-                <p>Vos données ont bien été enregistrées!</p>
-                <?php
-            } else {
-                //Affichage du contenu selon l'id de la page-->
-                echo $pages->page[$idPage - 1]->content;
-            }
-            ?>
+        <div class="jumbotron">
+            <!--Affichage du contenu selon l'id de la page-->
+            <?= $pages->page[$idPage - 1]->content ?>
         </div>
         <!--Footer-->
-        <div class="container-fluid bg-dark">
+        <div class="container-full bg-dark">
             <div class="row text-light">
                 <div class="col-md-6 border-right border-light">
                     <h4>A Propos</h4>
@@ -80,13 +64,13 @@ include 'validation.php';
                     <p class="foo">31 Rue Alexandre</p>
                     <p class="foo">80000 Amiens</p>
                     <p class="foo">Tel : +33 (0)3 22 72 22 22</p>
-                    <div class="foo">Email :<a href="mailto:contact@ocordo-travaux.fr" > contact@ocordo-travaux.fr</a></div>
+                    <p class="foo">Email : contact@ocordo-travaux.fr</p>
                 </div>
-            </div>     
+            </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script src="assets/js/script.js"></script>
+        <<script src="assets/js/script.js"></script>
     </body>
 </html>
